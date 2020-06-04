1a02d1c1417b03a42ecf9d0dfe36ad1055117a73
Valéria Todorovski Lucht de Carvalho <valeriatlcarvalho@users.noreply.github.com>
2020-06-03 23:08:32 -0300

    Initial commit

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Routes from './routes';
+
+// JSX: Sintaxe de XML dentro do JavaScript
+
+function App() {
+  return (
+    <>
+      <Routes />
+    </>
+  );
+}
+
+export default App;
