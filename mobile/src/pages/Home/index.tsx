ea4f4c9e61fd24add99b302acab90959d868ddf7
Valéria Todorovski Lucht de Carvalho <valeriatlcarvalho@users.noreply.github.com>
2020-06-05 12:16:35 -0300

    Finalização do mobile: integração com a API

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,41 +1,154 @@
-import React from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react';
 import { Feather as Icon } from '@expo/vector-icons';
-import { StyleSheet, Image, Text, ImageBackground, View } from 'react-native';
+import { StyleSheet, Image, Text, ImageBackground, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
+import PickerSelect from 'react-native-picker-select';
+import axios from 'axios';
+
+interface IBGEUFResponse {
+  id: number;
+  sigla: string;
+}
+
+interface IBGECityResponse {
+  id: number;
+  nome: string;
+}
+
+interface SelectItem {
+  label: string;
+  value: string;
+}
 
 const Home = () => {
+  const [ufs, setUfs] = useState<SelectItem[]>([]);
+  const [cities, setCities] = useState<SelectItem[]>([]);
   const navigation = useNavigation();
 
+  const [selectedUf, setSelectedUf] = useState('0');
+  const [selectedCity, setSelectedCity] = useState('0');
+
+  useEffect(() => {
+    async function loadUfs() {
+      await axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome').then(response => {
+        const ufInitials: SelectItem[] = response.data.map(uf => ({
+          label: uf.sigla,
+          value: uf.sigla,
+          key: String(uf.id),
+        }));
+        setUfs(ufInitials);
+      });
+    }
+    loadUfs();
+  }, []);
+
+  useEffect(() => {
+    if (selectedUf === '0') {
+      return;
+    }
+
+    async function loadCities() {
+      await axios
+        .get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios?orderBy=nome`)
+        .then(response => {
+          const cityNames = response.data.map(city => ({
+            label: city.nome,
+            value: city.nome,
+            key: String(city.id),
+          }));
+          setCities(cityNames);
+        });
+    }
+    loadCities();
+  }, [selectedUf]);
+
+  function handleSelectUf(value: any) {
+    const uf = value;
+    setSelectedUf(uf);
+  }
+
+  function handleSelectCity(value: any) {
+    const city = value;
+    setSelectedCity(city);
+  }
+
   function handleNavigateToPoints() {
-    navigation.navigate('Points');
+    navigation.navigate('Points', {
+      uf: selectedUf,
+      city: selectedCity,
+    });
   }
 
   return (
-    <ImageBackground
-      source={require('../../assets/home-background.png')}
-      style={styles.container}
-      imageStyle={{ width: 274, height: 368 }}
-    >
-      <View style={styles.main}>
-        <Image source={require('../../assets/logo.png')} />
-        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
-        <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
-      </View>
-
-      <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
-          <View style={styles.buttonIcon}>
-            <Text>
-              <Icon name="arrow-right" color="#fff" size={24} />
-            </Text>
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <ImageBackground
+        source={require('../../assets/home-background.png')}
+        style={styles.container}
+        imageStyle={{ width: 274, height: 368 }}
+      >
+        <View style={styles.main}>
+          <Image source={require('../../assets/logo.png')} />
+          <View>
+            <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+            <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
           </View>
-          <Text style={styles.buttonText}>
-            Entrar
-          </Text>
-        </RectButton>
-      </View>
-    </ImageBackground>
+        </View>
+
+        <View style={styles.footer}>
+          <PickerSelect
+            style={
+              Platform.OS === 'ios'
+                ? {
+                  inputIOS: styles.select,
+                  viewContainer: {
+                    borderRadius: 10,
+                  }
+                }
+                : {
+                  inputAndroid: styles.select,
+                  viewContainer: {
+                    borderRadius: 10,
+                  }
+                }
+            }
+            onValueChange={handleSelectUf}
+            placeholder={{ label: 'Selecione uma UF', value: null }}
+            items={ufs}
+          />
+          <PickerSelect
+            style={
+              Platform.OS === 'ios'
+                ? {
+                  inputIOS: styles.select,
+                  viewContainer: {
+                    borderRadius: 10,
+                  }
+                }
+                : {
+                  inputAndroid: styles.select,
+                  viewContainer: {
+                    borderRadius: 10,
+                  }
+                }
+            }
+            onValueChange={handleSelectCity}
+            placeholder={{ label: 'Selecione uma cidade', value: null }}
+            items={cities}
+          />
+          <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+            <View style={styles.buttonIcon}>
+              <Text>
+                <Icon name="arrow-right" color="#ffffff" size={24} />
+              </Text>
+            </View>
+            <Text style={styles.buttonText}>
+              Entrar
+            </Text>
+          </RectButton>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -69,11 +182,20 @@ const styles = StyleSheet.create({
 
   footer: {},
 
-  select: {},
+  select: {
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    height: 60,
+    fontSize: 16,
+    backgroundColor: '#ffffff',
+    borderWidth: 1,
+    borderColor: '#ffffff',
+    borderRadius: 10,
+  },
 
   input: {
     height: 60,
-    backgroundColor: '#FFF',
+    backgroundColor: '#ffffff',
     borderRadius: 10,
     marginBottom: 8,
     paddingHorizontal: 24,
@@ -102,7 +224,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     textAlign: 'center',
-    color: '#FFF',
+    color: '#ffffff',
     fontFamily: 'Roboto_500Medium',
     fontSize: 16,
   }
